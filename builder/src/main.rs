91cbd620984b78fbcc14094132b83f371693fd9a
Alan Smith <commit@alanwsmith.com>
2023-02-14 19:42:30 -0500

    Getting content

--- builder/src/main.rs
+++ builder/src/main.rs
@@ -12,6 +12,7 @@ use nom::sequence::preceded;
 use nom::IResult;
 // use nom::bytes::complete::take;
 use nom::character::complete::multispace0;
+use nom::character::complete::multispace1;
 use nom::combinator::map_parser;
 use nom::sequence::separated_pair;
 use nom::sequence::tuple;
@@ -19,22 +20,36 @@ use nom::sequence::tuple;
 use std::fs;
 
 fn main() {
-    let mut content = String::from("");
-    let file_data = fs::read_to_string("content/index.md").unwrap();
+    let mut contents = String::from("");
+    let file_data = fs::read_to_string("content/index.mdx").unwrap();
     let (input, title) = get_title(&file_data).unwrap();
     // println!("{}", title);
-    content.push_str("# ");
-    content.push_str(title);
-    output_file(&content);
+    contents.push_str("# ");
+    contents.push_str(title);
+
+    let (input, data) = get_content(input).unwrap();
+    contents.push_str("\n");
+    contents.push_str("\n");
+    contents.push_str(data);
+
+    output_file(&contents);
+}
+
+fn get_content(input: &str) -> IResult<&str, &str> {
+    // let (input, spaces) = space0(input)?;
+    // let (input, newline) = line_ending(input)?;
+    // let (input, spaces) = space0(input)?;
+    // let (input, newline) = line_ending(input)?;
+
+    let (input, newline) = multispace1(input)?;
+    let (input, header) = tag("---> CONTENT")(input)?;
+    let (input, content) = take_until("--->")(input)?;
+    Ok((input, content))
 }
 
 fn get_title(input: &str) -> IResult<&str, &str> {
     let (input, header) = tag("---> TITLE")(input)?;
-    let (input, spaces) = space0(input)?;
-    let (input, newline) = line_ending(input)?;
-    let (input, spaces) = space0(input)?;
-    let (input, newline) = line_ending(input)?;
-    let (input, spaces) = space0(input)?;
+    let (input, newline) = multispace1(input)?;
     let (input, title) = not_line_ending(input)?;
     Ok((input, title))
 }
--- src/index.md
+++ src/index.md
@@ -1,1 +1,17 @@
 # Introduction
+
+
+
+One of the ways I learn is by doing write-ups of
+whatever I'm working on. Right now, that's rust.
+I'm putting this site together to collect them.
+You may find them helpful if you learn the way
+I do.
+
+Most pages have examples on them where I show
+a full set of source code then show how to
+put it together step-by-step. Here's an example
+with "Hello World"
+
+<div id="source" />
+

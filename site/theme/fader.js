9e416c187f9431bac49994f11e6946dbdcf58395
Alan Smith <commit@alanwsmith.com>
2023-02-17 03:43:56 -0500

    Working on removing styles from lines

--- site/theme/fader.js
+++ site/theme/fader.js
@@ -17,15 +17,30 @@ const faderSelectors = [
   '.ace_rparen',
 ]
 
-const fadeLines = () => {
-  c.sets.forEach((lineSet, lineSetIndex) => {
-    console.log(lineSet)
-    console.log(c.playgrounds[lineSetIndex])
-    console.log(c.playgrounds[0].querySelector('.ace_line'))
-    c.playgrounds[0]
-      .querySelector('.ace_line')
-      .classList.remove('ace-tomorrow-night')
-    c.playgrounds[0].querySelector('.ace_line').classList.remove('ace_keyword')
+const prepLineFades = () => {
+  c.sets.forEach((faderSet, faderSetIndex) => {
+    faderSet.fadeLines.forEach((fadeLine, fadeLineIndex) => {
+      const playgroundNumber = faderSet + 1
+      const lineNumber = fadeLineIndex + 1
+
+      const fadeString = `.ace_line:nth-child(${lineNumber}) span { border: 1px solid red; }`
+      c.faderStyles.push(fadeString)
+
+      // const fadeElements = document.querySelectorAll(
+      //   '#content > main > pre:nth-child(6) > pre > code > div.ace_scroller > div > div.ace_layer.ace_text-layer > div > span'
+      // )
+      // fadeElements.forEach((fadeElement, fadeElementIndex) => {
+      //   fadeElement.style.border = '1px solid red'
+      // })
+    })
+
+    // console.log(lineSet)
+    // console.log(c.playgrounds[lineSetIndex])
+    // console.log(c.playgrounds[0].querySelector('.ace_line:nth-child(1)'))
+    // c.playgrounds[0]
+    //   .querySelector('.ace_line')
+    //   .classList.remove('ace-tomorrow-night')
+    // c.playgrounds[0].querySelector('.ace_line').classList.remove('ace_keyword')
   })
 }
 
@@ -33,12 +48,25 @@ const getPlaygrounds = () => {
   c.playgrounds = document.querySelectorAll('pre.playground')
 }
 
+const makeStyleSheet = () => {
+  c.styleOverride = document.createElement('style')
+  document.body.appendChild(c.styleOverride)
+}
+
+const updateStyles = () => {
+  c.styleOverride.innerHTML = c.faderStyles.join('\n')
+}
+
 const faderInit = () => {
   if (c) {
     console.log('Got fader config')
+    c.faderStyles = []
+    makeStyleSheet()
     getPlaygrounds()
-    fadeLines()
+    prepLineFades()
+    updateStyles()
     console.log(c)
   }
 }
+
 document.addEventListener('DOMContentLoaded', faderInit)
